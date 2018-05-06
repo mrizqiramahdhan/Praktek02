896dc2e3e38c3a8ed0d9139c9de50ea4320f96af
mrizqiramadhan <rizqi7muhammad@users.noreply.github.com>
2018-05-06 19:38:31 +0800

    Membuat Atribut dan Behavior

--- src/praktek02/Square.java
+++ src/praktek02/Square.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek02;
-
-/**
- *
- * @author WUMBO
- */
 public class Square {
+    double sisi;
+    double luas;
     
+    void cetakInfo(){
+        System.out.println("==============");
+        System.out.println("sisi : "+sisi);
+        System.out.println("luas : "+luas);
+        System.out.println("==============");
+    }
 }
